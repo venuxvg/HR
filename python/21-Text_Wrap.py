d6d5c8dfdb5614aadf56591c290076f02e41fa22
venuxvg <venuxvg@gmail.com>
2019-01-12 20:38:12 +0530

    Text Wrapping
    Filling is good

---
+++ python/21-Text_Wrap.py
@@ -0,0 +1,38 @@
+#!usr/bin/env python3
+
+
+# You are given a string S and width w. 
+# Your task is to wrap the string into a paragraph of width .
+
+# Input Format
+# The first line contains a string, S. 
+# The second line contains the width, w.
+
+# Constraints
+# 0<len(S)<1000
+# 0<w<len(S)
+# Output Format
+# Print the text wrapped paragraph.
+
+# Sample Input 0
+# ABCDEFGHIJKLIMNOQRSTUVWXYZ
+# 4
+
+# Sample Output 0
+# ABCD
+# EFGH
+# IJKL
+# IMNO
+# QRST
+# UVWX
+# YZ
+
+import textwrap
+def wrap(string, max_width):
+    result=textwrap.fill(string,max_width)
+    return result
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
